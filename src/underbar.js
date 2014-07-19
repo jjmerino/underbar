83eb4aa154a42fec2466014efcf013055fc8324f
Jose Merino <jomerinog@gmail.com>
2014-07-19 16:09:32 -0400

    implement defaults

--- src/underbar.js
+++ src/underbar.js
@@ -242,6 +242,15 @@ var _ = {};
   // Like extend, but doesn't ever overwrite a key that already
   // exists in obj
   _.defaults = function(obj) {
+    var objects = Array.prototype.slice.call(arguments,1);
+    return _.reduce(objects,function(last,item){
+      for(var key in item){
+        if(last[key]==undefined){
+          last[key] = item[key];
+        }
+      }
+      return last;
+    },obj);
   };
 
 
